c3795a2c5f086b9d28a6fa395d600408a00cae43
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-06 18:03:57 -0800

    checking start, end and middle index

--- binary-search.cpp
+++ binary-search.cpp
@@ -10,7 +10,7 @@ int main(int argc, char const* argv[]) {
   vector<int> inputArray2 = {-1, 0, 3, 5, 9, 12};
 
   int target1 = 512;
-  int target2 = 9;
+  int target2 = 0;
 
   int index1 = search(inputArray1, target1);
   int index2 = search(inputArray2, target2);
@@ -22,4 +22,39 @@ int main(int argc, char const* argv[]) {
 }
 
 int search(vector<int>& nums, int target) {
+  int startIndex = 0;
+  int endIndex = nums.size() - 1;
+  int midIndex = (startIndex + endIndex) / 2;
+  int midValue = nums[midIndex];
+
+  if (nums.size() == 0)
+    return -1;
+  if (nums[0] == target)
+    return 0;
+
+  while ((endIndex - startIndex) >= 0) {
+    // cout << "start index: " << startIndex << endl;
+    // cout << "end index: " << endIndex << endl;
+    // cout << "mid index: " << midIndex << endl;
+    // cout << "===============\n";
+
+    midValue = nums[midIndex];
+    if (midValue == target)
+      return midIndex;
+    if (nums[startIndex] == target)
+      return startIndex;
+    if (nums[endIndex] == target)
+      return endIndex;
+
+    if (target > midValue) {
+      startIndex = midIndex + 1;
+      endIndex--;
+    } else {
+      startIndex++;
+      endIndex = midIndex - 1;
+    }
+    midIndex = (startIndex + endIndex) / 2;
+  }
+
+  return -1;
 }
